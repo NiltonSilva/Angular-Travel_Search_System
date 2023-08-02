b67f95f4c3e7407895a2fc96df830d2260c5661d
Nilton S Ferreira <nsfgeo@hotmail.com>
2023-08-02 20:42:02 -0300

    feat: concluído o formulário de busca (origem, destino, data de ida e data de volta).

--- src/app/shared/card-busca/card-busca.component.ts
+++ src/app/shared/card-busca/card-busca.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-busca',
   templateUrl: './card-busca.component.html',
   styleUrls: ['./card-busca.component.scss']
 })
-export class CardBuscaComponent {
+export class CardBuscaComponent implements OnInit {
+
+  @Input()
+  src: string = '';
+  @Input()
+  alt: string = '';
+  @Input()
+  destino: string = '';
+  @Input()
+  valor: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void { }
 
 }
